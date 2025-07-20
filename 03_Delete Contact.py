638b8bf43681382fcce4ffff5e6c25a39821eaa3
sandeshdesai07 <136434467+sandeshdesai07@users.noreply.github.com>
2025-07-20 13:04:40 +0530

    Update 03_Delete Contact.py

--- 03_Delete Contact.py
+++ 03_Delete Contact.py
@@ -2,7 +2,7 @@ show_contacts(contacts)
 
 def delete_contact(contacts, name):
 
-    #your code here
+
     for i in range(len(contacts)):
       if contacts[i]['name'] == name:
         contacts.pop(i)

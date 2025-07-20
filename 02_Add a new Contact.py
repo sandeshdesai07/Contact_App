ed7e981f7bb22390c66646d515f5e0cf67dea04f
sandeshdesai07 <136434467+sandeshdesai07@users.noreply.github.com>
2025-07-20 13:01:21 +0530

    Create 02_Add a new Contact.py

---
+++ 02_Add a new Contact.py
@@ -0,0 +1,30 @@
+def add_contact(contacts):
+  c = 0
+  name ,number, email = input().split()
+  d = {}
+  if re.match(p_name, name):
+    c += 1
+    d['name'] = name
+
+  elif re.match(p_email, name):
+    c += 1
+    d['name'] = email
+
+  elif re.match(p_number, name):
+    c += 1
+    d['name'] = number
+
+  if c==3:
+    contacts.append(d)
+
+  else:
+    print('wrong input, try again')
+
+
+
+
+    ## your code here
+
+
+
+add_contact(contacts)

17922bc1cebb288bf1b9b7d64004f2dc2c10a034
sandeshdesai07 <136434467+sandeshdesai07@users.noreply.github.com>
2025-07-20 13:02:25 +0530

    Create 04_Update Contact details.py

---
+++ 04_Update Contact details.py
@@ -0,0 +1,13 @@
+def update_contact(contacts, name):
+  for i in range(len(contacts)):
+    if contacts[i]['name'] == name:
+      name, number, email = input().split()
+      contacts[i]['name'] = name
+      contacts[i]['number'] = number
+      contacts[i]['email'] = email
+
+
+    ## your code here
+
+name = 'mahesh'
+update_contact(contacts, name)
